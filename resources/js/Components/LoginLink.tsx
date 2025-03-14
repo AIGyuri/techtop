71aea70c900b669f971d288b4f8d539dc33c8cd1
AIGyuri <gyorgyszabo0421@gmail.com>
2025-03-14 09:15:44 +0100

    logo

---
+++ resources/js/Components/LoginLink.tsx
@@ -0,0 +1,11 @@
+import { UserRound } from 'lucide-react';
+
+const LoginLink = () => {
+  return (
+	<div className="relative">
+    <UserRound className='h5 w-5' />
+	 </div>
+  );
+};
+
+export default LoginLink;
