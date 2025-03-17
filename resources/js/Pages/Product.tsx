29a40816c6c494b4c33693bd77cae99e8e53deae
Virdzsi <148943834+pantya1petr522@users.noreply.github.com>
2025-03-17 13:28:05 +0100

    cartupdate2

--- resources/js/Pages/Product.tsx
+++ resources/js/Pages/Product.tsx
@@ -1,44 +1,73 @@
 
 import CartButton from '@/Components/CartButton';
 import { Product } from '@/lib/utils';
+import { Link } from '@inertiajs/react';
+import CartIcon from '@/Components/CartLink';
 
 
 const ProductListing = ({ product }: { product: Product }) => {
+
+    console.log(product.image)
+
     return (
-        <div className="rounded-lg border border-gray-200 bg-white p-6 shadow-sm flex items-center justify-center">
 
-            <div className="rounded-lg border border-gray-200 bg-white p-6 shadow-sm">
-                <div className="h-56 w-full">
-                    <img className="mx-auto h-full" src={product.image} alt={product.name} />
+        <main className='w-full'>
+            <nav className='flex justify-evenly items-center'>
+                <Link href="/">
+                    <h2 className="mt-6 mb-6 text-4xl font-semibold text-gray-900 text-center">
+                        Techtop
+                    </h2>
+                </Link>
+                <div className="flex items-center space-x-4" >
+                    <CartIcon />
                 </div>
-                <div className="pt-6">
-                    <div className="mb-4 flex items-center justify-between gap-4">
+            </nav>
+            <div>
+                <div className="rounded-lg border border-gray-200 bg-white p-6 shadow-sm flex items-center justify-center">
+                    <div className="rounded-lg border border-gray-200 bg-white p-6 shadow-sm">
+                        <div className="h-80 w-full">
+                            <img className="mx-auto h-full" src={`/storage/${product.image}`} alt={product.image} />
+                        </div>
+                        <div className="pt-6">
+                            <div className="mb-4 flex items-center justify-between gap-4">
+                                <span className="me-2 rounded bg-primary-100 px-2.5 py-0.5 text-xl font-bold text-primary-800 ">
+                                    {product.name}
+                                </span>
 
-                        <span className="me-2 rounded bg-primary-100 px-2.5 py-0.5 text-xs font-medium text-primary-800">
-                            {product.name}
-                        </span>
+                            </div>
+
+                            <div className="mb-4 flex items-center justify-between gap-4">
+
+                                <span className="me-2 rounded bg-primary-100 px-2.5 py-0.5 text-s font-medium text-primary-800">
+                                    {product.description}
+                                </span>
+                            </div>
+                            <CartButton product={{ ...product, quantity: 1 }} />
+                        </div>
+
+                        <div style={{ marginTop: '2rem', textAlign: 'center' }}>
+                            <Link
+                                href="/products"
+                                style={{ backgroundColor: '#1d3557', color: 'white', padding: '0.75rem 1.5rem', borderRadius: '8px', cursor: 'pointer', transition: 'background-color 0.3s' }}
+                            >
+                                Vissza a termékekhez
+                            </Link>
+                        </div>
                     </div>
 
-                    <div className="mb-4 flex items-center justify-between gap-4">
-
-                        <span className="me-2 rounded bg-primary-100 px-2.5 py-0.5 text-xs font-medium text-primary-800">
-                            {product.description}
-                        </span>
-                    </div>
-                    <CartButton product={{ ...product, quantity: 1 }} />
-                </div>
-            </div>
 
 
-            {/* <img src={product.image} alt={product.name} className="product-image" />
+                    {/* <img src={product.image} alt={product.name} className="product-image" />
             <div className="product-details">
                 <h1 className="product-name">{product.name}</h1>
                 <p className="product-description">{product.description}</p>
                 <p className="product-price">{product.price} Ft</p>
                 <button className="order-button">Megrendelés</button>
             </div> */}
-        </div>
-        
+                </div>
+            </div>
+        </main>
+
     );
 };
 
