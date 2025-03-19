d92c20a377ac1d76b73aa15ae58eae127ea60b02
Virdzsi <148943834+pantya1petr522@users.noreply.github.com>
2025-03-19 10:11:53 +0100

    loginregisterupdate

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -126,6 +126,16 @@ export default function Login({
                         </PrimaryButton>
                     </div>
                 </form>
+
+                  {/* Vissza gomb a regisztrációs form alatt */}
+                  <div className="mt-4 flex justify-right">
+                    <Link
+                        href="/" // Vissza a főoldalra
+                        className="inline-block bg-blue-500 hover:bg-blue-300 text-white px-4 py-2 rounded-lg shadow-md"
+                    >
+                        Vissza
+                    </Link>
+                </div>
             </div>
         </div>
     );
